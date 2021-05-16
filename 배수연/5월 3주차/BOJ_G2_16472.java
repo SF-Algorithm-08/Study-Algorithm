00a6014d5b25d4a06c2a9579226ea3e434a23951
sy0227 <tv0227@naver.com>
2021-05-16 20:03:19 +0900

    Update BOJ_G2_16472.java

--- 배수연/5월 3주차/BOJ_G2_16472.java
+++ 배수연/5월 3주차/BOJ_G2_16472.java
@@ -15,22 +15,6 @@ public class BOJ_G2_16472 {
 		
 		int[] alpha = new int[26];
 		int left = 0, cnt = 0, ans = 0;
-//		alpha[str.charAt(0)-'a']++;
-//		
-//		while(right<str.length()) {
-//			right++;
-//			if(right == str.length()) break;
-//			
-//			// 종류가 N개 넘으면 left 개수 빼주고 다른 문자 나올 때까지 left 한 칸 뒤로
-//			while(cnt>N) {
-//				if(alpha[str.charAt(left)-'a']-- == 0) cnt--;
-//				left++;
-//			}
-//			// right 문자열 추가해주기
-//			if(alpha[str.charAt(right)-'a']++ == 1) cnt++;
-//			// 매번 사용한 문자열의 길이가 N 이하면 ans 갱신
-//			if(cnt<=N) ans = Math.max(ans, right-left+1);
-//		}
 		for(int right = 0 ; right<str.length() ; right++) {
 			int temp = str.charAt(right)-'a';
 			if(alpha[temp]>0) alpha[temp]++;
